5b66d16b4581954d069243fa0823be6035fd521a
Lab-Game <eric.lehman@gmail.com>
2025-11-07 16:19:05 -0800

    fixing some tests related to suit order

--- src/test.c
+++ src/test.c
@@ -14,7 +14,7 @@ int POV = 0;  // Print debug from current player's point of view
 
 void Cards_test(void) {
     puts("\nTesting Cards...");
-    Cards cards = Cards_fromString("aC TC 5D 6D 2H JH 6S KS AS");
+    Cards cards = Cards_fromString("aC TC 5D 6D 6S KS AS 2H JH");
     printf("Expected:  aC TC 5D 6D 2H JH 6S KS AS\n");
     printf("Actual:    ");
     Cards_print(cards);
@@ -23,7 +23,7 @@ void Cards_test(void) {
 
     // Remove a card and check points again.
     Cards_remove(&cards, Cards_fromString("aC")); // remove aC
-    printf("Expected:  TC 5D 6D 2H JH 6S KS AS\n");
+    printf("Expected:  TC 5D 6D 6S KS AS 2H JH\n");
     printf("Actual:    ");
     Cards_print(cards);
     printf("\n");
@@ -31,24 +31,17 @@ void Cards_test(void) {
 
     // Add some cards and check points again.
     Cards_add(&cards, Cards_fromString("JC QC")); // add JC QC
-    printf("Expected:  TC JC QC 5D 6D 2H JH 6S KS AS\n");
+    printf("Expected:  TC JC QC 5D 6D 6S KS AS 2H JH\n");
     printf("Actual:    ");
     Cards_print(cards);
     printf("\n");
     assert(Cards_points(cards) == 85);
 
-    // Display the full deck.
-    Cards fullDeck = FULL_DECK;
-    printf("Expected:  2C 3C 4C 5C 6C 7C 8C 9C TC JC QC KC AC 2D 3D 4D 5D 6D 7D 8D 9D TD JD QD KD AD 2H 3H 4H 5H 6H 7H 8H 9H TH JH QH KH AH 2S 3S 4S 5S 6S 7S 8S 9S TS JS QS KS AS\n");
-    printf("Actual:    ");
-    Cards_print(fullDeck);
-    printf("\n");
-
     // Test adding low aces.
     Cards handWithHighAces = Cards_fromString("AC AD AH AS 5H 6H");
     Cards handWithLowAces = Cards_lowerAces(handWithHighAces);
-    printf("Expected with low aces:  aC AC aD AD aH 5H 6H AH aS AS\n");
-    printf("Actual with low aces:    ");
+    printf("Expected:  aC AC aD AD aS AS aH 5H 6H AH\n");
+    printf("Actual:    ");
     Cards_print(handWithLowAces);
     printf("\n");
 
